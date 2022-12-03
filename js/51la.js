821c7aa55e958c9ec0459046c4d25303809481e0
unknown <chongqing0111@gmail.com>
2022-12-03 21:26:15 +0800

    Site updated: 2022-12-03 21:26:14

---
+++ js/51la.js
@@ -0,0 +1,2 @@
+LA.init({ id: "JtSPqyOae1d7xW2k", ck: "JtSPqyOae1d7xW2k" });
+new LingQue.Monitor().init({ id: "JthY1SOnNqvDDBR8" });
---
+++ talk/index.md
@@ -0,0 +1,8 @@
+---
+title: talk
+date: 2022-12-01 20:43:22
+top_img:
+comments:
+type: "talk"
+---
+
