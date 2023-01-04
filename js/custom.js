fe4ab3351998bf1c81c95e74f6eb63e4ad9316ed
chongqing0111 <chongqing0111@gmail.com>
2023-01-04 18:05:31 +0000

    ok

--- js/custom.js
+++ js/custom.js
@@ -44,8 +44,19 @@ function changeBg (s, flag) {
   if (!flag) { saveData('blogbg', s) }
 }
 
-
-
+// 随机访问一篇文章的实现
+// 发现有时会和当前页面重复，加一个判断
+function randomPost() {
+  fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+      let ls = data.querySelectorAll('url loc');
+      while (true) {
+          let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+          if (location.href == url) continue;
+          location.href = url;
+          return;
+      }
+  })
+}
 
 
 
